67a0b2669e72c13a1b8b5a126141183a42296f22
Brad Gardner <bradgardner@sevenhillstechnology.com>
2018-01-17 15:56:43 -0500

    refactoring server setup

--- server/index.ts
+++ server/index.ts
@@ -5,28 +5,55 @@ import {registerApiRoutes} from './api';
 import * as winston from 'winston';
 require('winston-daily-rotate-file');
 
-function configureLogs() {
-    winston.add(winston.transports.DailyRotateFile, {
-        name: "appFile",
-        filename: "./logs/app.log",
-        datePattern: 'yyyy-MM-dd.',
-        prepend: true,
-        level: "debug",
-        humanReadableUnhandledException: true,
-        handleExceptions: true,
-        json: true
-    });
+export class Server {
+    server:any;
+
+    constructor() {
+        const file_transport = new (winston.transports.DailyRotateFile)({
+            name: "appFile",
+            filename: "./logs/app.log",
+            datePattern: 'yyyy-MM-dd.',
+            prepend: true,
+            level: "debug",
+            humanReadableUnhandledException: true,
+            handleExceptions: true,
+            json: true
+        });
+    
+        const console_transport = new (winston.transports.Console)({
+            prepend: true,
+            level: "debug",
+            humanReadableUnhandledException: true,
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        });
+
+        const logger = new (winston.Logger)({
+            transports: [
+                file_transport,
+                console_transport
+            ]
+        });
+
+        this.server = new Hapi.Server({ port: 3000, host: 'localhost' });
+        this.server.app.logger = logger;
+
+        
+        
+    }
+
+    async startServer(isTestMode:boolean) : Promise<void> {
+        //register standard plugins
+        await this.server.register({plugin: require('hapijs-status-monitor')});
+
+        //register standard api routes
+        registerApiRoutes(this.server);
+    
+        if (!isTestMode) {
+            await server.start();
+            this.logger.info(`Server running at: ${server.info.uri}`);
+        }
+    }
 }
 
-async function startServer() : Promise<void> {
-    const server = new Hapi.Server({ port: 3000, host: 'localhost' });
-
-    registerApiRoutes(server);
-    await server.register({plugin: require('hapijs-status-monitor')});
-
-    await server.start();
-    winston.info(`Server running at: ${server.info.uri}`);
-}
-
-configureLogs();
-startServer();
