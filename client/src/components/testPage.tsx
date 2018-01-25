addd8516ad755c8135390a20f9dbf3fca2205232
ZakFahey <z.fahey@gmail.com>
2018-01-25 15:56:07 -0500

    REP-61: added test page that interfaces with the db

--- client/src/components/testPage.tsx
+++ client/src/components/testPage.tsx
@@ -2,24 +2,44 @@ import React from 'react';
 import {TestService} from '../services';
 
 interface IState {
-    testVal?: string;
+    testVal: string;
+    noteToAdd: string;
+    notes: string[];
 }
 
 export class TestPage extends React.Component<{}, IState> {
+    private testService: TestService;
+
     constructor(props : Readonly<{}>) {
         super(props);
-        this.state = {testVal: "-"};
-        const testService = new TestService();
-        testService.getTestValue().then(val => {
+        this.state = {testVal: "-", noteToAdd: "", notes: []};
+        this.testService = new TestService();
+        this.testService.getTestValue().then(val => {
             this.setState({testVal: val});
-            console.log(val);
+        });
+        this.testService.getNotes().then(val => {
+            this.setState({notes: val});
         });
     }
+    handleChange(e: any) {
+        this.setState({ noteToAdd: e.target.value });
+    }
+
+    async addNote(e: any) : Promise<void> {
+        const note = this.state.noteToAdd;
+        this.setState({ noteToAdd: "" });
+        const newNote = await this.testService.addNote(note);
+        this.setState({notes: this.state.notes.concat([newNote])})
+    }
+
     render() {
         return (
             <div>
                 <h1>Test</h1>
                 <p><b>Value from API:</b> {this.state.testVal}</p>
+                <input type="text" value={this.state.noteToAdd} onChange={this.handleChange.bind(this)} />
+                <button onClick={this.addNote.bind(this)}>Add note</button>
+                {this.state.notes.map(n => <p>{n}</p>)}
             </div>
         );
     }
--- client/src/services/testService.ts
+++ client/src/services/testService.ts
@@ -6,4 +6,19 @@ export class TestService {
         const json = await response.json();
         return json.test;
     }
+
+    async getNotes() : Promise<string[]> {
+        const response = await fetch('http://localhost:3000/notes');
+        return await response.json();
+    }
+
+    async addNote(note: string) : Promise<string> {
+        const payload = {note};
+        const response = await fetch(
+            'http://localhost:3000/add-note',
+            {method: 'POST', body: JSON.stringify(payload)}
+        );
+        const json = await response.json();
+        return json.text;
+    }
 }
