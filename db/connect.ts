c41b39db9d8e01f82a3b81b558e32fc98fe3e367
ZakFahey <z.fahey@gmail.com>
2018-02-16 15:21:13 -0500

    Fixed db not working

--- db/connect.ts
+++ db/connect.ts
@@ -2,12 +2,11 @@ import * as Knex from 'knex';
 import {Model} from 'objection';
 import * as nconf from "nconf";
 import * as path from "path";
+import {BaseModel} from 'db-starter';
 
 nconf.argv().env().file('development', path.resolve(__dirname, 'config', 'development.json'));
 const knex = Knex(nconf.get('knex'));
-
-Model.knex(knex);
-console.log('Model binded');
+BaseModel.knex(knex);
 
 export {knex};
 export {Model};
