7a1fb2d2edbac3881032725db3cbb7341d33ca5c
Brad Gardner <bradgardner@sevenhillstechnology.com>
2018-02-21 07:02:31 -0500

    restored test page as plain react example

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Switch, NavLink, Route } from 'react-router-dom';
 import StatusPage from './components/statusPage';
 import TestPage from './components/testPage';
+import {TestPageReact} from './components/testPageReact';
 import NotFoundPage from './components/notFoundPage';
 
 class App extends React.Component {
@@ -12,14 +13,16 @@ class App extends React.Component {
     return (
       <div>
         <div className="app-nav">
-          <NavLink exact to="/" activeStyle={activeStyle}>Status</NavLink>
-          <NavLink exact to="/test" activeStyle={activeStyle}>Test</NavLink>
+          <NavLink exact to="/" activeStyle={activeStyle}>Status</NavLink> | 
+          <NavLink exact to="/testRedux" activeStyle={activeStyle}>Test Redux</NavLink> | 
+          <NavLink exact to="/testReact" activeStyle={activeStyle}>Test React</NavLink> | 
           <NavLink exact to="/nowhere" activeStyle={activeStyle}>Test default route</NavLink>
         </div>
         <div className="app-content">
           <Switch>
             <Route exact path="/" component={StatusPage} />
-            <Route exact path="/test" component={TestPage} />
+            <Route exact path="/testRedux" component={TestPage} />
+            <Route exact path="/testReact" component={TestPageReact} />
             <Route component={NotFoundPage} />
           </Switch>
         </div>
