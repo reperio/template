5e547f5c43445a8043f909ee430007bd19d3ec9c
Brad Gardner <bradgardner@sevenhillstechnology.com>
2018-02-27 19:35:58 -0500

    sass added and removed dist files

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -11,20 +11,20 @@ class App extends React.Component {
   render() {
     const activeStyle = { color: 'red' };
     return (
-      <div>
-        <div className="app-nav">
-          <NavLink exact to="/" activeStyle={activeStyle}>Status</NavLink> | 
-          <NavLink exact to="/testRedux" activeStyle={activeStyle}>Test Redux</NavLink> | 
-          <NavLink exact to="/testReact" activeStyle={activeStyle}>Test React</NavLink> | 
-          <NavLink exact to="/nowhere" activeStyle={activeStyle}>Test default route</NavLink>
+      <div className="app-main">
+        <div className="app-sidebar">
+            <NavLink exact to="/" activeStyle={activeStyle}>Status</NavLink><br/>
+            <NavLink exact to="/testRedux" activeStyle={activeStyle}>Test Redux</NavLink><br/>
+            <NavLink exact to="/testReact" activeStyle={activeStyle}>Test React</NavLink><br/>
+            <NavLink exact to="/nowhere" activeStyle={activeStyle}>Test default route</NavLink><br/>
         </div>
         <div className="app-content">
-          <Switch>
-            <Route exact path="/" component={StatusPage} />
-            <Route exact path="/testRedux" component={TestPage} />
-            <Route exact path="/testReact" component={TestPageReact} />
-            <Route component={NotFoundPage} />
-          </Switch>
+            <Switch>
+              <Route exact path="/" component={StatusPage} />
+              <Route exact path="/testRedux" component={TestPage} />
+              <Route exact path="/testReact" component={TestPageReact} />
+              <Route component={NotFoundPage} />
+            </Switch>
         </div>
       </div>
     );
