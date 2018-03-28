eec2802e1650f6d1869e1d79f1f209307027b124
Brad Gardner <bradgardner@sevenhillstechnology.com>
2018-03-27 21:26:25 -0600

    navbar layout

--- client/src/components/navMenu/navMenu.tsx
+++ client/src/components/navMenu/navMenu.tsx
@@ -6,10 +6,46 @@ import NavMenuLoginLink from "./navMenuLoginLink";
 import NavMenuLogOutLinkContainer from "../../containers/navMenuLogOutLinkContainer";
 
 const NavMenu = (props: any) => (
-    <div>
-        {props.authSession.isAuthenticated ? <LinkContainer to="/home"><NavItem>Home</NavItem></LinkContainer> : null}
-        {props.authSession.isAuthenticated ? <NavMenuLogOutLinkContainer/> : <NavMenuLoginLink/>}
+    <div className="app-sidebar">
+        <div className="nav-side-menu">
+            <div className="brand">Brand Logo</div>
+            <i className="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+        
+                <div className="menu-list">
+        
+                    <ul id="menu-content" className="menu-content collapse out">
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-dashboard fa-lg"></i> Dashboard
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-dashboard fa-lg"></i> Dashboard
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-dashboard fa-lg"></i> Dashboard
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-dashboard fa-lg"></i> Dashboard
+                            </a>
+                        </li>
+                        
+                    </ul>
+            </div>
+        </div>
     </div>
+    // <div>
+    //     {props.authSession.isAuthenticated ? <LinkContainer to="/home"><NavItem>Home</NavItem></LinkContainer> : null}
+    //     {props.authSession.isAuthenticated ? <NavMenuLogOutLinkContainer/> : <NavMenuLoginLink/>}
+    // </div>
 );
 
 export default NavMenu;
