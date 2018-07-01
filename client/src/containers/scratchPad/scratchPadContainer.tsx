0dc8c86733d6f40a478eddd83a070f2125b63101
Brian Adams <brian@brianadams.codes>
2018-07-01 11:17:52 -0400

    Finish up the active button styling.

--- client/src/containers/scratchPad/scratchPadContainer.tsx
+++ client/src/containers/scratchPad/scratchPadContainer.tsx
@@ -9,7 +9,18 @@ class ScratchPadContainer extends React.Component {
                 <div className="row">
                     <div className="col-md-6">
                         <div>Buttons</div>
-                        <button className="r-form-control r-btn r-neutral">Neutral</button>
+                        <div className="col-md-3">
+                            <button className="r-form-control r-btn r-neutral">Neutral</button>
+                        </div>
+                        <div className="col-md-3">
+                            <button className="r-form-control r-btn r-danger">Danger</button>
+                        </div>
+                        <div className="col-md-3">
+                            <button className="r-form-control r-btn r-success">Success</button>
+                        </div>
+                        <div className="col-md-3">
+                            <button className="r-form-control r-btn r-cancel">Cancel</button>
+                        </div>
                     </div>
                 </div>
                 <div className="row">
