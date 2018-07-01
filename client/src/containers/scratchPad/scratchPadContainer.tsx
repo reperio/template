cbb43e8a5d8193231e3af4bfc64b41aec464be6f
Brian Adams <brian@brianadams.codes>
2018-07-01 15:48:49 -0400

    Get radio buttons and checkboxes complete.

--- client/src/containers/scratchPad/scratchPadContainer.tsx
+++ client/src/containers/scratchPad/scratchPadContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Field} from 'redux-form'
 
 class ScratchPadContainer extends React.Component {
     props: any;
@@ -68,6 +69,48 @@ class ScratchPadContainer extends React.Component {
                     </div>
                 </div>
                 <div className="col-md-6">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div>Checkboxes/Radio Buttons</div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <label className="r-checkbox-container">
+                                        Checkbox Label
+                                        <input type="checkbox" name="tstChk" id="" />
+                                        <span className="r-checkbox"></span>
+                                    </label><br/>
+                                    <label className="r-checkbox-container">
+                                        Disabled Checkbox Label
+                                        <input type="checkbox" name="tstChk2" disabled id="" />
+                                        <span className="r-checkbox"></span>
+                                    </label>
+                                </div>
+                                <div className="col-md-6">
+                                    <label className="r-checkbox-container">
+                                        Radio Label 1
+                                        <input type="radio" name="rad"/>
+                                        <span className="r-radio-checkmark"></span>
+                                    </label><br/>
+                                    <label className="r-checkbox-container">
+                                        Radio Label 2
+                                        <input type="radio" name="rad" />
+                                        <span className="r-radio-checkmark"></span>
+                                    </label>
+                                    <br />
+                                    <label className="r-checkbox-container">
+                                        Disabled Radio 1
+                                        <input type="radio" name="radDis" checked={true} disabled />
+                                        <span className="r-radio-checkmark"></span>
+                                    </label><br/>
+                                    <label className="r-checkbox-container">
+                                        Disabled Radio 2
+                                        <input type="radio" name="radDis" disabled />
+                                        <span className="r-radio-checkmark"></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 
                 
